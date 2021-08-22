4e0bd57b1d1e6906fef5e87fe66a7fe7d91f1f31
Ryan Rhodes <ryan@ryanrhodes.io>
2021-08-21 23:24:38 -0400

    fix(schema): updated pricing

--- schemas/documents/services.js
+++ schemas/documents/services.js
@@ -4,73 +4,108 @@ import { GiLargePaintBrush, GiFoxTail } from 'react-icons/gi';
 import { FaRegClipboard as icon } from 'react-icons/fa';
 
 export default {
-  name: 'services',
-  title: 'Services',
-  type: 'document',
-  icon,
-  fields: [
-    {
-      name: 'category',
-      title: 'Category',
-      type: 'string',
-      options: {
-        list: ['Styling', 'Color', 'Extensions'],
-      },
-      validation: Rule => Rule.required().error('A category must be choosen'),
-    },
-    {
-      name: 'title',
-      title: 'Title',
-      type: 'string',
-      validation: Rule => Rule.required().error('Service must have a title'),
-    },
-    {
-      name: 'description',
-      title: 'Description',
-      type: 'text',
-      validation: Rule =>
-        Rule.required().error('Service must have a description'),
-    },
-    {
-      name: 'cost',
-      title: 'Cost',
-      type: 'number',
-      validation: Rule =>
-        Rule.required()
-          .positive()
-          .integer()
-          .error('Service must have a valid cost'),
-    },
-    {
-      name: 'time',
-      title: 'Time',
-      type: 'number',
-      description: 'Estimated time in minutes',
-      validation: Rule =>
-        Rule.required()
-          .positive()
-          .integer()
-          .error('Service must have a valid time'),
-    },
-  ],
-  preview: {
-    select: {
-      title: 'title',
-      cost: 'cost',
-      category: 'category',
-    },
-    prepare({ title, cost, category }) {
-      return {
-        title: title ? title : 'New Service',
-        subtitle: cost ? `$${cost}` : '',
-        media: (
-          <>
-            {category === 'Styling' && <ImScissors />}
-            {category === 'Color' && <GiLargePaintBrush />}
-            {category === 'Extensions' && <GiFoxTail />}
-          </>
-        ),
-      };
-    },
-  },
+	name: 'services',
+	title: 'Services',
+	type: 'document',
+	icon,
+	fields: [
+		{
+			name: 'category',
+			title: 'Category',
+			type: 'string',
+			options: {
+				list: ['Styling', 'Color', 'Extensions'],
+			},
+			validation: Rule =>
+				Rule.required().error('A category must be choosen'),
+		},
+		{
+			name: 'title',
+			title: 'Title',
+			type: 'string',
+			validation: Rule =>
+				Rule.required().error('Service must have a title'),
+		},
+		{
+			name: 'description',
+			title: 'Description',
+			type: 'text',
+			validation: Rule =>
+				Rule.required().error('Service must have a description'),
+		},
+		{
+			name: 'costType',
+			title: 'Cost Type',
+			type: 'string',
+			options: {
+				list: [
+					{ title: 'Fixed', value: 'Fixed' },
+					{ title: 'Dynamic', value: 'Dynamic' },
+					{ title: 'Ranged', value: 'Ranged' },
+				],
+				layout: 'radio',
+				direction: 'horizontal',
+			},
+		},
+		{
+			name: 'fixedCost',
+			title: 'Fixed Cost',
+			type: 'number',
+			hidden: ({ document }) => {
+				return document.costType !== 'Fixed';
+			},
+		},
+		{
+			name: 'dynamicCost',
+			title: 'Dynamic Cost',
+			type: 'number',
+			hidden: ({ document }) => {
+				return document.costType !== 'Dynamic';
+			},
+		},
+		{
+			name: 'rangedCost',
+			title: 'Ranged Cost',
+			type: 'object',
+			hidden: ({ document }) => {
+				return document.costType !== 'Ranged';
+			},
+			fields: [
+				{
+					name: 'minCost',
+					title: 'Min Cost',
+					type: 'number',
+				},
+				{
+					name: 'maxCost',
+					title: 'Max Cost',
+					type: 'number',
+				},
+			],
+		},
+		{
+			name: 'time',
+			title: 'Time',
+			type: 'number',
+			description: 'Estimated time in minutes',
+		},
+	],
+	preview: {
+		select: {
+			title: 'title',
+			category: 'category',
+		},
+		prepare({ title, cost, category }) {
+			return {
+				title: title ? title : 'New Service',
+				media: (
+					<>
+						{category === 'Styling' && <ImScissors />}
+						{category === 'Color' && <GiLargePaintBrush />}
+						{category === 'Extensions' && <GiFoxTail />}
+					</>
+				),
+			};
+		},
+	},
 };
