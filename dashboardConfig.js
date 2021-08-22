c13abfcf2451513e0f21ad139ee6cba993e6392e
Ryan Rhodes <ryan@ryanrhodes.io>
2021-08-22 10:20:12 -0400

    config fix

--- dashboardConfig.js
+++ dashboardConfig.js
@@ -10,6 +10,7 @@ export default {
 						apiId: '84ef6638-9b5d-4575-97b8-f6648cfb2ba6',
 						buildHookId: '60c9233e0b7a0e391d889ad2',
 						name: 'jessica-website-2021',
+						url: 'https://jesserhodes.style',
 					},
 				],
 			},
