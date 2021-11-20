ed7021b0410c926871326a893c0e630078aa41fa
Ryan Rhodes <ryan@ryanrhodes.io>
2021-11-20 07:59:42 -0500

    fix(netlify): removed netlify plugin

--- dashboardConfig.js
+++ dashboardConfig.js
@@ -1,19 +1,3 @@
 export default {
-	widgets: [
-		{
-			name: 'netlify',
-			options: {
-				title: 'Website',
-				sites: [
-					{
-						title: 'Production',
-						apiId: '84ef6638-9b5d-4575-97b8-f6648cfb2ba6',
-						buildHookId: '60c9233e0b7a0e391d889ad2',
-						name: 'jessica-website-2021',
-						url: 'https://jesserhodes.style',
-					},
-				],
-			},
-		},
-	],
+	widgets: [],
 };
